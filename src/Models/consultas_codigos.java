639e845f06fa384a8eb0dea76862f87b355d6b9a
Francisco Reza <fran.js_@hotmail.com>
2021-06-18 09:23:37 -0500

    avance avanzado de cosas avanzadas XD

--- src/Models/consultas_codigos.java
+++ src/Models/consultas_codigos.java
@@ -3,10 +3,16 @@ package Models;
 
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.Barcode128;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.Desktop;
+import java.awt.HeadlessException;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +23,8 @@ import javax.swing.JOptionPane;
 
 public class consultas_codigos extends conexion {
 
+        PdfWriter pdf;
+        Document doc = new Document();
     
     public ArrayList obtenerModelos(){
         ArrayList lista = new ArrayList();
@@ -54,21 +62,35 @@ public class consultas_codigos extends conexion {
         }
         
         try {
-            
-            Document doc = new Document();
-            PdfWriter pdf = PdfWriter.getInstance(doc, new FileOutputStream("codigos.pdf"));
+            int i = 0;
+            pdf = PdfWriter.getInstance(doc, new FileOutputStream("codigobarrasnuevo.pdf"));
             doc.open();
-            
             Barcode128 code128 = new Barcode128();
-            code128.setCode(resultado_codigo);
-            Image img = code128.createImageWithBarcode(pdf.getDirectContent(), BaseColor.BLACK, BaseColor.BLACK);
-            doc.add(img);
             
+            while (i < 18) {                
+                
+                code128.setCode(resultado_codigo);
+                code128.setCodeType(Barcode128.CODE128);
+                
+
+                Image img = code128.createImageWithBarcode(pdf.getDirectContent(), BaseColor.BLACK, BaseColor.BLACK);
+                img.scalePercent(250);
+                
+                Paragraph titulo = new Paragraph("XD", new com.itextpdf.text.Font(com.itextpdf.text.Font.FontFamily.COURIER, 5));
+                float scaler = ((doc.getPageSize().getWidth() - doc.leftMargin() - doc.rightMargin() - 0 / img.getWidth()) * 60);
+                
+                
+                doc.add(titulo);
+                doc.add(img);
+                doc.add(new Paragraph(" "));
+            i++; //iterador
+            }
             doc.close();
             
-            JOptionPane.showMessageDialog(null, "Codigos de Barra Generados Correctamente!");
+            JOptionPane.showMessageDialog(null, "Codigos de Barra Generados Correctamente!");           
+            Desktop.getDesktop().open(new File("codigobarrasnuevo.pdf"));
             
-        } catch (Exception ex) {
+        } catch (DocumentException | HeadlessException | IOException ex) {
             System.out.println("Error 6164:" + ex);
         }      
     }
